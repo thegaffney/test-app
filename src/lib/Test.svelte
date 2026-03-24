1e5f16194797d57f43f932aebc32ce6553c24210
Michael Gaffney <gaffnm@mohave.gov>
2026-03-24 10:32:49 -0700

    Another error in page.svelte

--- src/lib/Test.svelte
+++ src/lib/Test.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    let {children} = $props()
+    let {children, response} = $props()
 
     const normalJS: string = ''
 </script>
-<button onclick={(e: MouseEvent)=>{e.stopPropagation();}}>{@render children()}</button>
+<button onclick={(e: MouseEvent)=>{e.stopPropagation(); response()}}>{@render children()}</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
-<script>
+<script lang="ts">
     import Test from "$lib/Test.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Test>Test Component</Test>
+<Test response={()=>{
+    alert('clicked');
+    const test: string = ''
+}}>Test Component</Test>
