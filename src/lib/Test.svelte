86cf3efac29d5fe2eff65b1c7c5d2ee69f8284c0
Michael Gaffney <gaffnm@mohave.gov>
2026-03-24 10:00:30 -0700

    Initial commit

---
+++ src/lib/Test.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    let {children} = $props()
+</script>
+<button onclick={(e: MouseEvent)=>{e.stopPropagation();}}>{@render children()}</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Test from "$lib/Test.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Test>Test Component</Test>
